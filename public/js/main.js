0780e7a9ec81eaac70fb878f4cce82caec77b977
Akim Delli <akim@ubuntu-linux>
2014-06-24 22:56:50 -0400

    added the jquery, underscore library files

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-require(["helper/util"], function(util) {
+require(["modules/map"], function(map) {
     //This function is called when scripts/helper/util.js is loaded.
     //If util.js calls define(), then this function is not fired until
     //util's dependencies have loaded, and the util argument will hold
