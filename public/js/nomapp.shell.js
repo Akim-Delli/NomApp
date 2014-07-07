32fc24844f8f922569ae8ff105b5f3bd03d7e6c5
Akim  Delli <akim_delli@hotmail.com>
2014-07-06 23:20:48 -0400

    implemented chat slider: click to retract or extend

--- public/js/nomapp.shell.js
+++ public/js/nomapp.shell.js
@@ -18,12 +18,25 @@ define(['jquery'], function($){
             '</div>' +
             	'<div class="nomapp-shell-foot"></div>' +
             	'<div class="nomapp-shell-chat"></div>' +
-            '<div class="nomapp-shell-modal"></div>'
+            '<div class="nomapp-shell-modal"></div>',
+
+
+        chat_extend_time	: 1000,
+        chat_retract_time	: 300,
+        chat_extend_height	: 450,
+        chat_retract_height	: 15,
+        chat_extended_title: 'Click to retract',
+        chat_retracted_title: 'Click to extend'
  	},
- 		stateMap = { $container : null},
+ 		stateMap = {
+ 			$container : null,
+ 			is_chat_retracted : true
+ 		},
  		jqueryMap = {},
 
  		setJqueryMap,
+ 		toggleChat,
+ 		onclickChat,
  		initModule;
  	//------------------ END MODULE SCOPE VARIABLE ---------------------------//
 
@@ -34,20 +47,95 @@ define(['jquery'], function($){
  	// Begin DOM method /setJqueryMap
  	setJqueryMap = function () {
  		var $container = stateMap.$container;
- 		jqueryMap = { $container : $container };
+ 		jqueryMap = {
+ 			$container : $container,
+ 			$chat: $container.find( '.nomapp-shell-chat')
+ 		};
  	};
  	//End DOM method /setJqueryMap
+
+ 	// Begin DOM method /toggleChat/
+ 	// Purpose : Extends or retracts chat slider
+ 	// Arguments :
+ 	//   * do_extend - if true, extends slider; if false retracts
+ 	//   * callback - optional function to execute at end of animation
+ 	// Settings :
+ 	//	 * chat_extend_time, chat_retract_time
+ 	//   * chat_extend_height, chat_retract_height
+ 	// Returns : boolean
+ 	//	 * true  - slider animation activated
+ 	//   * false - slider animation not activated
+ 	// State : sets stateMap.is_chat_retracted
+ 	//   * true - slider is retracted
+ 	//   * false - slider is extended
+ 	toggleChat = function ( do_extend, callback ) {
+ 		var
+ 			px_chat_ht = jqueryMap.$chat.height(),
+ 			is_open    = px_chat_ht === configMap.chat_extend_height,
+ 			is_closed  = px_chat_ht === configMap.chat_retract_height,
+ 			is_sliding = ! is_open && !is_closed;
+
+ 		// avoid race condition
+ 		if ( is_sliding ){ return false; }
+
+ 		// Begin extend chat slider
+ 		if ( do_extend ) {
+ 			jqueryMap.$chat.animate(
+ 				{ height : configMap.chat_extend_height },
+ 				configMap.chat_extend_time,
+ 				function () {
+ 					jqueryMap.$chat.attr(
+ 						'title', configMap.chat_extended_title
+ 					);
+ 					stateMap.is_chat_retracted = false;
+ 					if ( callback ) { callback ( jqueryMap.$chat ); }
+ 				}
+ 			);
+ 			return true;
+ 		}
+ 		// End extend chat slider
+
+ 		// Begin retract chat slider
+ 		jqueryMap.$chat.animate(
+ 			{ height : configMap.chat_retract_height },
+ 				configMap.chat_retract_time,
+ 				function () {
+ 					jqueryMap.$chat.attr(
+ 						'title', configMap.chat_retracted_title
+ 					);
+ 					stateMap.is_chat_retracted = true;
+ 					if ( callback ) { callback ( jqueryMap.$chat ); }
+ 				}
+ 			);
+ 		return true;
+ 		// End retract chat slider
+ 	};
+ 	//End DOM method /toggleChat/
  	//------------------ END DOM METHODS -------------------------------------//
 
 	//------------------ BEGIN EVENT HANDLERS --------------------------------//
+ 	onclickChat = function () {
+ 		toggleChat( stateMap.is_chat_retracted );
+ 		return false;
+ 	};
  	//------------------ END EVENT HANDLERS ----------------------------------//
 
  	//------------------ BEGIN PUBLIC METHODS --------------------------------//
  	// Begin Public method /initModule/
  	initModule = function ( $container) {
+ 		// load HTML and map JQuery collections
  		stateMap.$container = $container;
  		$container.html( configMap.main_html );
  		setJqueryMap();
+
+ 		stateMap.is_chat_retracted = true;
+ 		jqueryMap.$chat
+ 			.attr( 'title', configMap.chat_retracted_title)
+ 			.click( onclickChat);
+
+ 		// test toggle
+ 		//setTimeout( function () { toggleChat( true); }, 3000);
+ 		//setTimeout( function () { toggleChat( false); }, 8000);
  	};
  	// End Public method /initModule/
 
