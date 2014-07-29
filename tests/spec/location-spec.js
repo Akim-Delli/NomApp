0b2df42261c1e98def7b3158238baf591582bb96
Akim  Delli <akim_delli@hotmail.com>
2014-07-28 22:10:48 -0400

    add jasmine location tests & Travis CI tests configuration

---
+++ tests/spec/location-spec.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+
+// We need a database connection
+mongoose.connect('mongodb://localhost/test');
+
+var location = require('../../models/location')(mongoose);
+
+describe('location', function() {
+	it ('should save a location (DeviceId, Longitude and Latitude)', function(){
+
+		var status;
+		location.record( 1, 40.00000,-80.00000, test);
+		function test () {
+			status = "saved";
+		};
+
+		// wait for async call to be finished
+	    waitsFor(function() {
+	        return status !== undefined;
+	    }, 'should return a status that is not undefined', 1000);
+
+	    // run the assertion after status has been set
+	    runs(function() {
+	        expect(status).toEqual("saved");
+	    });
+	});
+
+	it ('should retrieve the latest location save', function(){
+		var latestLocation,
+			status;
+		latestLocation = location.findLatest(test);
+		function test () {
+			status = 'retrieved';
+		};
+
+		// wait for async call to be finished
+	    waitsFor(function() {
+	        return status !== undefined;
+	    }, 'should return a status that is not undefined', 1000);
+
+	    // run the assertion after response has been set
+	    runs(function() {
+	        expect(status).toEqual("retrieved");
+	    });
+	} );
+});
